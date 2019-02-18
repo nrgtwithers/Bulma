f9e41bde860a8d198ebd3ed5157a410fea0b0c6d
Nina Withers <ninarosewithers@gmail.com>
2019-02-18 17:32:36 -0500

    changes to contact

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+console.log('jellow')
+
+var btn = document.querySelector('#showModal');
+var modalDlg = document.querySelector('#image-modal');
+var imageModalCloseBtn = document.querySelector('#image-modal-close');
+btn.addEventListener('click', function(){
+  modalDlg.classList.add('is-active');
+});
+
+imageModalCloseBtn.addEventListener('click', function(){
+  modalDlg.classList.remove('is-active');
+});
